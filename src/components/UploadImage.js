c1f1674290eb8ee16f199f20461c4aa3abbbb5d0
Anson Cheng <Anson.12666@icloud.com>
2023-12-07 03:34:10 +0800

    feat: support upload image to ipfs

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,20 +1,75 @@
-import { useState } from "react";
-import { create as ipfsHttpClient } from "ipfs-http-client";
-
-const projectSecretKey = process.env.IPFSapiKey;
+import React, { useRef, useState } from "react";
+import axios from "axios";
+const FormData = require("form-data");
 
 export const UploadImage = () => {
-  const [uploadedImages, setUploadedImages] = useState([]);
+  const inputFile = useRef();
 
-  console.log(process.env);
+  const pinFileToIPFS = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
 
-  const ipfs = ipfsHttpClient({
-    // https://console.web3.storage/
-    url: "https://api.filebase.io/v1/ipfs",
-    headers: {
-      projectSecretKey,
-    },
-  });
+    // check if file is an image
+    if (!file.type.match(/image.*/)) {
+      window.alert("This file is not an image");
+      return;
+    }
 
-  return <button className="uploadButton">Upload NFT Image</button>;
+    // check file size < 1MB
+    if (file.size > 1048576) {
+      window.alert("File size must be less than 1MB");
+      return;
+    }
+
+    const pinataMetadata = JSON.stringify({ name: file.name });
+    formData.append("pinataMetadata", pinataMetadata);
+
+    const pinataOptions = JSON.stringify({ cidVersion: 0 });
+    formData.append("pinataOptions", pinataOptions);
+
+    try {
+      const res = await axios.post(
+        // https://app.pinata.cloud/
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        formData,
+        {
+          maxBodyLength: "Infinity",
+          headers: {
+            "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+            Authorization: `Bearer ${process.env.REACT_APP_IPFSapiKey}`,
+          },
+        }
+      );
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      inputFile.current.value = null;
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      pinFileToIPFS(file);
+    }
+  };
+
+  return (
+    <div className="uploadImage">
+      <input
+        type="file"
+        id="file"
+        ref={inputFile}
+        onChange={handleFileChange}
+        style={{ display: "none" }}
+      />
+      <button
+        className="uploadButton"
+        onClick={() => inputFile.current.click()}
+      >
+        Upload NFT Image
+      </button>
+    </div>
+  );
 };
