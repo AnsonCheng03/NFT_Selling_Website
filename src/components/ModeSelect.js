e3c508655070bcac4e17249d85486806ebe9b4f3
Anson Cheng <Anson.12666@icloud.com>
2023-12-07 18:20:06 +0800

    Update button labels in ModeSelect component

--- src/components/ModeSelect.js
+++ src/components/ModeSelect.js
@@ -7,7 +7,7 @@ export const ModeSelect = ({ mode, setMode }) => {
         }
         onClick={() => setMode("create")}
       >
-        Create
+        Create NFTs
       </button>
 
       <button
@@ -16,7 +16,7 @@ export const ModeSelect = ({ mode, setMode }) => {
         }
         onClick={() => setMode("buy")}
       >
-        Buy
+        View NFTs
       </button>
     </div>
   );
